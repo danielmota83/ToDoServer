cc681c35293f6491fb4df89058c705b50a81e263
Daniel Mota <101141570+danielmota83@users.noreply.github.com>
2023-03-05 13:58:16 -0300

    feat: add auth files

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(email: string, pass: string): Promise<any> {
+    const user = await this.usersService.findByEmail(email);
+    const isMatch = await bcrypt.compare(pass, user.password);
+
+    if (user && isMatch) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user._doc.email, sub: user._doc._id };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
